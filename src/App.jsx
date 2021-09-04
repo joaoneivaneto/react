28f68fa1eaeec7e52d7727bc9567546ea2134bbb
marcarAtendimento <joaoneivanetos@gmail.com>
2021-09-04 14:22:23 -0300

    consumindo API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tasks from './components/Tasks';
 import {v4 as uuidv4} from 'uuid';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import './App.css';
 import AddTask from './components/AddTask';
 import TaskDetails from './components/TaskDetails';
-
+import axios from 'axios';
 
 
 
@@ -29,7 +29,18 @@ const App = () => {
     },
     
   ]);
+  
+  useEffect(()=>{
+      const fetchTasks = async () =>{
+          const {data} = await axios.get(
+            "https://jsonplaceholder.cypress.io/todos?_limit=10"
+          );
 
+          setTasks(data);
+      };
+
+      fetchTasks();
+  }, []);
   const handleTaskClick = (taskId) => {
       const newTasks = tasks.map( task =>{
         if(task.id === taskId) return {...task, completed : !task.completed}
