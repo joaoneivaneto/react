09cc1357dca84296cde4b899aeac6ece3eef3343
marcarAtendimento <joaoneivanetos@gmail.com>
2021-09-04 13:49:03 -0300

    botoes 100% funcionando

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./task.css";
-import {CgClose, CgInfo} from 'react-icons/cg'
+import {CgClose, CgInfo} from 'react-icons/cg';
+import { useHistory } from 'react-router-dom';
+
 const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
+    const history = useHistory();
+    const handleTaskDetailsClick = () => {
+        history.push(`/${task.title}`);
+    }
     return(
         <div 
-        className="task-conteiner"
+        className="task-container"
         style={task.completed ? { borderLeft: "6px solid chartreuse" } : {} }
         >
             
@@ -13,13 +19,13 @@ const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
             </div>
             <div className="buttons-container">
                 <button className="remove-task-button" onClick={() => handleTaskDeletion(task.id)}><CgClose/></button>
-                <button className="see-task-details-button" ><CgInfo/></button>
+                <button className="see-task-details-button" onClick={handleTaskDetailsClick} ><CgInfo/></button>
             </div>
             
         </div>
 
     )
-    //return  <div className="task-conteiner">{task.title}</div> ;
+    
 }
  
 export default Task;
--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import Button from "./Button";
 import  "./TaskDetails.css";
 
 const TaskDetails = () =>{
     const params = useParams();
 
-    console.log(params);
+    const history = useHistory();
+
+    const hanbleBackButtonClick = () =>{
+        history.goBack();
+    }
     return (
         <>
             <div className="back-button-container">
-                <Button>Voltar</Button> 
+                <Button onClick={hanbleBackButtonClick}>Voltar</Button> 
             </div>
             <div className="task-details-container">
                 <h2>{params.taskTitle}</h2>
